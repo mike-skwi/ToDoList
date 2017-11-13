16faf613d99fa4b1353ffccd5a695fa4e2fdd3e5
Mike Skwierawski <6mikeski13@gmail.com>
2017-11-13 05:35:35 +0000

    fixes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,14 +2,18 @@ class TasksController < ApplicationController
     def create
       @task = Task.new(task_params)
       if @task.save
+        flash[:notice] = 'Item has been added'
+
         redirect_to root_path
       else
+        flash[:alert] = "Text field cannot be blank"
         redirect_to root_path
       end
     end
   def destroy
       @task = Task.find(params[:id])
       @task.destroy
+      flash[:notice] = 'Item has been removed'
       redirect_to root_path
   
   end
